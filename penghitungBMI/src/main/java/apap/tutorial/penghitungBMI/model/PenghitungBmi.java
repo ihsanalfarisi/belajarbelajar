caebdde6cb83d0cb9b296ecb8e56b6eeaed413f1
Muhammad Ihsan Al Farisi <muhammad.ihsan06@ui.ac.id>
2022-09-14 16:56:46 +0700

    feat(tutorial1): penghitung BMI

---
+++ penghitungBMI/src/main/java/apap/tutorial/penghitungBMI/model/PenghitungBmi.java
@@ -0,0 +1,35 @@
+package apap.tutorial.penghitungBMI.model;
+import apap.tutorial.penghitungBMI.PenghitungBmiApplication;
+
+import java.io.Serializable;
+
+
+public class PenghitungBmi implements  Serializable{
+    private Double beratBadan;
+    private Double tinggiBadan;
+    private String kategori;
+
+    public PenghitungBmi(String beratBadan, String tinggiBadan){
+        this.beratBadan = Double.parseDouble((beratBadan));
+        this.tinggiBadan = Double.parseDouble((tinggiBadan))/100;
+    }
+    public String hitungBmi() {
+        double hasilBmi = beratBadan / (Math.pow(tinggiBadan, 2));
+        if (hasilBmi < 18.5) {
+            this.kategori = "Berat badan anda kurang";
+
+        } else if (hasilBmi >= 188.5 && hasilBmi <= 22.9) {
+            this.kategori = "Berat badan anda normal";
+        }
+        else if (hasilBmi>22.9 && hasilBmi <= 29.9){
+            this.kategori = "Berat badan anda berlebih (kecendurangan obesitas)";
+        }
+        else {
+            this.kategori = "Anda obesitas";
+        }
+        return String.format("%.2f", hasilBmi);
+    }
+    public double getWeight() {return beratBadan;}
+    public double getHeight() {return tinggiBadan;}
+    public String getKategori (){return kategori;}
+}
